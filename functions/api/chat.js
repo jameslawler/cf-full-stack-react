8739e22c6f3462f0c8a1dd320ebe2722028945ba
James Lawler <services@jameslawler.com>
2025-04-14 21:38:24 +0100

    Update prompt

--- functions/api/chat.js
+++ functions/api/chat.js
@@ -1,8 +1,8 @@
 export async function onRequestGet({ env }) {
   const response = await env.AI.run('@cf/meta/llama-2-7b-chat-int8', {
     messages: [
-      { role: 'system', content: 'You are a helpful assistant.' },
-      { role: 'user', content: "what is the capital of Portugal?" },
+      { role: 'system', content: 'You are a teacher of European Portuguese. Provide Feedback on the sentences to be a helpful teacher. Identify grammar and spelling issues.' },
+      { role: 'user', content: "a minha mãe e Sofia e ela tinha 45 anos." },
     ],
   });
 
