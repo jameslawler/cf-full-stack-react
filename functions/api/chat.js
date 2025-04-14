4e732460f8d56c1dfa4acf5cfb3508c2a14c410b
James Lawler <services@jameslawler.com>
2025-04-14 21:17:28 +0100

    Fix ai

--- functions/api/chat.js
+++ functions/api/chat.js
@@ -1,9 +1,7 @@
 export async function onRequestGet({ env }) {
-  const ai = new Ai(env.AI);
-
   const { prompt } = await request.json();
 
-  const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+  const response = await env.AI.run('@cf/meta/llama-2-7b-chat-int8', {
     messages: [
       { role: 'system', content: 'You are a helpful assistant.' },
       { role: 'user', content: "what is the capital of Portugal?" },
