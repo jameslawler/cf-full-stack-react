884234cc148957da7b71312d6ae8cb02f4a5d6d3
James Lawler <services@jameslawler.com>
2025-04-15 15:12:14 +0100

    Add post endpoint

--- functions/api/chat.js
+++ functions/api/chat.js
@@ -10,3 +10,18 @@ export async function onRequestGet({ env }) {
     headers: { 'Content-Type': 'application/json' },
   });
 }
+
+export async function onRequestPost({ request, env }) {
+  const data = await request.json(); // or formData/text based on content-type
+
+  const response = await env.AI.run('@cf/meta/llama-2-7b-chat-int8', {
+    messages: [
+      { role: 'system', content: 'Analyse my European Portuguese writing and correct any grammar or spelling mistakes.'},
+      { role: 'user', content: data.question}
+    ]
+  });
+
+  return new Response(JSON.stringify(response), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
